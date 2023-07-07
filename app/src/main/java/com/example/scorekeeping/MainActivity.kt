38ef8a9687b9f95a74b17ed6c7653d42bfb0fc26
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-07-07 11:33:22 -0400

    Added onClickListeners for team 1 to add and reduce score

--- app/src/main/java/com/example/scorekeeping/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeping/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.scorekeeping
 
 import androidx.appcompat.app.AppCompatActivity
+
+
 import android.os.Bundle
+import com.example.scorekeeping.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var team1Score = 0
+    private var team2Score = 0
+    private var scoreChange = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.team1IncreaseButton.setOnClickListener {
+            team1Score += scoreChange
+            binding.team1ScoreTextView.text = team1Score.toString()
+        }
+
+        binding.team1DecreaseButton.setOnClickListener {
+            team1Score -= scoreChange
+            binding.team1ScoreTextView.text = team1Score.toString()
+        }
     }
 }
